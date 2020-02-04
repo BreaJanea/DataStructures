48aa8f4b2b66b688e125e0a470e72c0327051f7c
Brea Spencer <breaspencer@quickenloans.com>
2020-02-04 08:19:34 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static Stack<char> stack = new Stack<char>();
+        static char[] arr;
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word you would like to reverse: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("This is your reverse: " + Reverse(input));
+        }
+
+        static string Reverse(string str)
+        {
+             arr = str.ToCharArray();
+             string reverseWord = null;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+              stack.Push(arr[i]);
+            }
+
+            while (stack.Count > 0)
+            {
+               reverseWord += stack.Pop();       
+            }
+            return reverseWord;
+        }   
+    }   
+}
